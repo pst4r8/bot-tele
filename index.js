172614a916e2e787ba6f00e40afe9993991c276d
Pst4r8 <94172938+pst4r8@users.noreply.github.com>
2026-03-23 21:38:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,44 @@ import 'dotenv/config'
 import { makeTelegramSocket } from "tg-socket"
 
 const sock = makeTelegramSocket({
-  token: process.env.BOT_TOKEN,
-  polling: true
+	token: process.env.BOT_TOKEN,
+	polling: true
 })
 
 sock.command("start", async msg => {
-	console.log(JSON.stringify(msg,null,2))
-  await msg.reply("Hello from Telegram-Socket 👋")
+	await msg.reply("Hello from Telegram-Socket 👋")
 })
 
+sock.command("igstalk", async (msg, q) => {
+	console.log(JSON.stringify(msg,null,2))
+	console.log(JSON.stringify(q,null,2))
+	axios.get(`https://api.alyachan.dev/api/searching/ig/stalk?username=${q.replace(/^@/, '')}`, {
+		headers: { 
+			'Authorization': 'Bearer sk_prod_d71375171514d055a8d866302c92a4f5',
+			'Content-Type': 'application/json'
+	  }
+	}).then(async ({ data }) => {
+		if (data.status == false) return msg.reply("Username tidak ditemukan!")
+		const item = data.data
+		var caption = `*Username:* ${item.username}\n`
+		caption += `*Nickname:* ${item.fullname}\n`
+		caption += `*Following:* ${item.following}\n`
+		caption += `*Followers:* ${item.followers}\n`
+		caption += `*Private:* ${item.is_private}\n`
+		caption += `*Posts:* ${item.post}\n`
+		caption += `*Bio:* ${item.bio}\n`
+		caption += `*Link:* https://instagram.com/${q.replace(/^@/, '')}`
+		await msg.sendPhoto(msg.chat.id, item.profile, caption)
+	}).catch(async (err) => {
+		if (err?.response?.status === 404) {
+			return msg.reply("Username tidak ditemukan!")
+		} else {
+			console.error(`*[${command}] [${moment().tz('Asia/Jakarta').format('HH:mm:ss')}]* => ${String(err)}`)
+			await msg.reply("Terjadi error pada fitur!")
+		}
+	})
+})
+	
+	
+	
 sock.start()
