a8d97939721926b6dc535688275953c0dd61655f
Pst4r8 <94172938+pst4r8@users.noreply.github.com>
2026-03-23 20:55:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const { makeTelegramSocket } = require("@xrelly-spec/telegram-socket")
 
+const bot = makeTelegramSocket({
+  token: process.env.BOT_TOKEN,
+  polling: true
+})
+
+bot.command("start", async msg => {
+  await msg.reply("Hello from Telegram-Socket 👋")
+})
+
+bot.start()
